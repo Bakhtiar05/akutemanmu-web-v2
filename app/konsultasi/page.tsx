fa9e78ef077e59b3cc24ea244f8be0e42e57695b
Bakhtiar Kamil <Bakhtiarkamil0510@gmail.com>
2026-06-30 14:27:52 +0700

    sepertinya gassken

--- app/konsultasi/page.tsx
+++ app/konsultasi/page.tsx
@@ -14,9 +14,7 @@ export default function KonsultasiPage() {
           <h1 className="text-3xl md:text-[42px] font-display font-bold text-[#0F172A] animate-fade-enter leading-snug md:leading-tight tracking-tight max-w-[300px] md:max-w-none">
             Pesan Jadwal Konsultasi
           </h1>
-          <p className="text-[15px] md:text-lg text-[#64748B] w-11/12 md:max-w-[650px] animate-fade-enter leading-relaxed line-clamp-3" style={{ animationDelay: '100ms' }}>
-            Langkah awal menuju kesehatan mental yang lebih baik. Ceritakan apa yang Anda rasakan, dan kami akan membantu menemukan jalan keluarnya.
-          </p>
+
         </div>
 
         <BookingWizard />
